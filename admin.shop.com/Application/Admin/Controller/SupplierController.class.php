4ab24f090502d5b87ddbb8053dc4b832992ad080
许坤 <kunx-edu@qq.com>
2016-03-07 11:38:34 +0800

    讲解供货商的添加表单

--- admin.shop.com/Application/Admin/Controller/SupplierController.class.php
+++ admin.shop.com/Application/Admin/Controller/SupplierController.class.php
@@ -1,12 +1,31 @@
 <?php
 namespace Admin\Controller;
 class SupplierController extends \Think\Controller{
+    protected function _initialize(){
+        $meta_titles = array(
+            'index'=>'供货商管理',
+            'add'=>'添加供货商',
+            'edit'=>'修改供货商',
+            'delete'=>'删除供货商',
+        );
+        $meta_title = isset($meta_titles[ACTION_NAME])?$meta_titles[ACTION_NAME]:'供货商管理';
+        $this->assign('meta_title',$meta_title);
+    }
+
+
     /**
      * 展示供货商的列表
      */
     public function index(){
         $model = D('Supplier');
         $this->assign('rows',$model->select());
+        $this->display();
+    }
+    
+    /**
+     * 新增供货商
+     */
+    public function add(){
         $this->display();
     }
 }
