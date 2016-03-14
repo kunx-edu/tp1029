cd32d7e04ed08e12a9ce00897b009fbf7bf6a8eb
许坤 <kunx-edu@qq.com>
2016-03-14 11:49:02 +0800

    编辑权限

--- admin.shop.com/Application/Admin/Controller/PermissionController.class.php
+++ admin.shop.com/Application/Admin/Controller/PermissionController.class.php
@@ -1,9 +1,9 @@
 <?php
 
-
 namespace Admin\Controller;
 
-class PermissionController extends \Think\Controller{
+class PermissionController extends \Think\Controller {
+
     private $_model = null;
 
     protected function _initialize() {
@@ -17,40 +17,66 @@ class PermissionController extends \Think\Controller{
         $this->assign('meta_title', $meta_title);
         $this->_model = D('Permission'); //由于所有的操作都需要用到模型,我们在初始化方法中创建
     }
-    
+
     /**
      * 权限列表页
      * TODO:都没完成
      */
-    public function index(){
-        $this->assign('rows',  $this->_model->getList());
+    public function index() {
+        $this->assign('rows', $this->_model->getList());
         $this->display();
     }
-    
-    
-    public function add(){
-        if(IS_POST){
-            if($this->_model->create()===false){
-                $this->error($this->_model->getError());
-            }
-            if($this->_model->addPermission()===false){
-                $this->error($this->_model->getError());
-            }
-            
-            $this->success('添加权限成功',U('index'));
-            
-        }else{
-            //展示现有权限列表
-            $rows = M('Permission')->select();
-            $top_node = array(
-                'id'=>0,
-                'name'=>'请选择',
-                'parent_id'=>'',
 
-            );
-            array_unshift($rows,$top_node);
-            $this->assign('rows',json_encode($rows));
+    public function add() {
+        if (IS_POST) {
+            if ($this->_model->create() === false) {
+                $this->error($this->_model->getError());
+            }
+            if ($this->_model->addPermission() === false) {
+                $this->error($this->_model->getError());
+            }
+
+            $this->success('添加权限成功', U('index'));
+        } else {
+            $this->_before_view();
             $this->display();
         }
     }
+
+    /**
+     * 编辑权限
+     * @param type $id
+     */
+    public function edit($id) {
+        if (IS_POST) {
+            if ($this->_model->create() === false) {
+                $this->error($this->_model->getError());
+            }
+            if ($this->_model->updatePermission() === false) {
+                $this->error($this->_model->getError());
+            }
+
+            $this->success('修改权限成功', U('index'));
+        } else {
+            $this->_before_view();
+            $this->assign('row',$this->_model->find($id));
+            $this->display('add');
+        }
+    }
+
+    /**
+     * 获取权限列表,以便ztree生成树
+     */
+    private function _before_view() {
+        //展示现有权限列表
+        $rows     = $this->_model->select();
+        $top_node = array(
+            'id'        => 0,
+            'name'      => '请选择',
+            'parent_id' => '',
+        );
+        array_unshift($rows, $top_node);
+        $this->assign('rows', json_encode($rows));
+    }
+
 }
--- admin.shop.com/Application/Admin/Model/PermissionModel.class.php
+++ admin.shop.com/Application/Admin/Model/PermissionModel.class.php
@@ -1,36 +1,80 @@
 <?php
 
-
 namespace Admin\Model;
 
-class PermissionModel extends \Think\Model{
+class PermissionModel extends \Think\Model {
+
     protected $_validate = array(
-        array('name','require','权限名称不能为空',self::EXISTS_VALIDATE,'',self::MODEL_BOTH),
-        array('parent_id','require','父级权限不能为空',self::EXISTS_VALIDATE,'',self::MODEL_BOTH),
+        array('name', 'require', '权限名称不能为空', self::EXISTS_VALIDATE, '', self::MODEL_BOTH),
+        array('parent_id', 'require', '父级权限不能为空', self::EXISTS_VALIDATE, '', self::MODEL_BOTH),
     );
-    
+
     /**
      * 添加权限,使用nestedsets
      * 由于是新增权限,所以直接调用nestedsets的insert即可
      */
-    public function addPermission(){
+    public function addPermission() {
         unset($this->data['id']);
-        $db_mysql = D('DbMysql','Logic');
-        $table_name = $this->trueTableName;
+        $db_mysql    = D('DbMysql', 'Logic');
+        $table_name  = $this->trueTableName;
         $nested_sets = new \Admin\Service\NestedSetsService($db_mysql, $table_name, 'lft', 'rght', 'parent_id', 'id', 'level');
-        if($nested_sets->insert($this->data['parent_id'], $this->data, 'bottom') === false){
+        if ($nested_sets->insert($this->data['parent_id'], $this->data, 'bottom') === false) {
             $this->error = '添加权限失败';
             return false;
         }
         return true;
     }
-    
+
     /**
      * 获取权限列表
      * @return type
      */
-    public function getList(){
-        $cond = array('status'=>array('gt',0));
+    public function getList() {
+        $cond = array('status' => array('gt', 0));
         return $this->where($cond)->select();
     }
+
+    /**
+     * 修改权限.
+     * @return boolean
+     */
+    public function updatePermission() {
+        //获取原来的父级节点
+        $request_data = $this->data;
+        //编辑分类的时候判断指定的父级分类下有没有同名分类
+        $cond         = array(
+            'parent_id' => $request_data['parent_id'],
+            'name'      => $request_data['name'],
+            'id'        => array('neq', $request_data['id']),
+        );
+        //查看是否有记录
+        if ($this->where($cond)->count()) {
+            $this->error = '已经存在同名权限';
+            return false;
+        }
+
+
+
+        $old_parent_id = $this->getFieldById($request_data['id'], 'parent_id');
+        //由于如果没有改变父级分类,moveUnder将会返回false,所以我们先判断是否改变了父级分类
+        if ($old_parent_id !== $request_data['parent_id']) {
+            //创建具体的sql执行的对象
+            $db          = D('DbMysql', 'Logic');
+            $table_name  = $this->trueTableName; //获取数据表的名字
+            //创建用于生成sql结构的对象
+            $nested_sets = new \Admin\Service\NestedSetsService($db, $table_name, 'lft', 'rght', 'parent_id', 'id', 'level');
+            if ($nested_sets->moveUnder($request_data['id'], $request_data['parent_id'], 'bottom') === false) {
+                $this->error = '修改父级权限失败';
+                return false;
+            }
+        }
+
+        if ($this->save() === false) {
+            $this->error = '修改权限失败';
+            return false;
+        }
+
+        return true;
+    }
+
 }
