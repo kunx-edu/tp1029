4715adfc31f9f36b713da12e2486d0befb02c77a
许坤 <kunx-edu@qq.com>
2016-03-15 16:59:20 +0800

    解决框架集缓存的方案1

--- admin.shop.com/index.php
+++ admin.shop.com/index.php
@@ -11,17 +11,7 @@
 //curl_init();
 //phpinfo();
 //exit;
-////设置此页面的过期时间(用格林威治时间表示)，只要是已经过去的日期即可。 
-//header("Expires: Mon, 26 Jul 1970 05:00:00 GMT"); 
-//
-////设置此页面的最后更新日期(用格林威治时间表示)为当天，可以强制浏览器获取最新资料 
-//header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-//
-////告诉客户端浏览器不使用缓存，HTTP 1.1 协议 
-//header("Cache-Control: no-cache,no-store, must-revalidate"); 
-//
-////告诉客户端浏览器不使用缓存，兼容HTTP 1.0 协议 
-//header("Pragma: no-cache");
+
 
 // 应用入口文件
 
@@ -39,5 +29,7 @@ define('APP_PATH',ROOT_PATH.'/Application/');
 
 // 引入ThinkPHP入口文件
 require dirname(ROOT_PATH) . '/ThinkPHP/ThinkPHP.php';
-
+//header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+//header("Cache-Control: no-cache, must-revalidate");
+//header("Pragma: no-cache");
 // 亲^_^ 后面不需要任何代码了 就是如此简单
