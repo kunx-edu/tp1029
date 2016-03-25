7babcc56000a50413e618854fc72bb6dd87a94cc
许坤 <kunx-edu@qq.com>
2016-03-25 15:11:01 +0800

    解决并发问题，当有人购买某商品时，其余用户等待锁定释放

--- www.shop.com/Application/Home/Model/OrderInfoModel.class.php
+++ www.shop.com/Application/Home/Model/OrderInfoModel.class.php
@@ -3,12 +3,13 @@
 namespace Home\Model;
 
 class OrderInfoModel extends \Think\Model {
+
     private static $statuses = array(
-        0=>'已关闭',
-        1=>'待支付',
-        2=>'待发货',
-        3=>'待确认',
-        4=>'已完成',
+        0 => '已关闭',
+        1 => '待支付',
+        2 => '待发货',
+        3 => '待确认',
+        4 => '已完成',
     );
 
     public function addOrder() {
@@ -16,18 +17,34 @@ class OrderInfoModel extends \Think\Model {
         $userinfo          = login();
         $shopping_car_info = D('ShoppingCar')->getShoppingCarInfo();
 
-        
-        //更新库存
-        if($this->_refreshStock($shopping_car_info)===false){
+        //解决并发问题，检查库存有没有被锁定
+        if($this->_checkAllGoodsIsLocked($shopping_car_info)===false){
+//            $this->_removeGoodsLock($shopping_car_info);
             $this->rollback();
             return false;
         }
         
+        //更新库存
+        if ($this->_refreshStock($shopping_car_info) === false) {
+            $this->_removeGoodsLock($shopping_car_info);
+            $this->rollback();
+            return false;
+        }
+
         $address_id   = I('post.address_id');
+        //判断是否选择了收货地址
+        if(!$address_id){
+            $this->error = '请选择收货地址';
+            $this->_removeGoodsLock($shopping_car_info);
+            $this->rollback();
+            return false;
+            
+        }
         $address_info = M('Address')->find($address_id);
         //保存订单基本信息
         if (($order_id     = $this->_addOrderInfo($shopping_car_info, $userinfo, $address_info)) === false) {
             $this->error = '创建订单失败';
+            $this->_removeGoodsLock($shopping_car_info);
             $this->rollback();
             return false;
         }
@@ -35,6 +52,7 @@ class OrderInfoModel extends \Think\Model {
         //保存订单详情
         if ($this->_addOrderDetail($order_id, $shopping_car_info) === false) {
             $this->error = '保存订单详情失败';
+            $this->_removeGoodsLock($shopping_car_info);
             $this->rollback();
             return false;
         }
@@ -42,31 +60,33 @@ class OrderInfoModel extends \Think\Model {
         //开票
         if (($invoice = $this->_addInvoice($order_id, $shopping_car_info, $userinfo, $address_info)) === false) {
             $this->error = '保存发票失败';
+            $this->_removeGoodsLock($shopping_car_info);
             $this->rollback();
             return false;
         }
         //回写到订单表中
         $data = array(
-            'id'=>$order_id,
-            'invoice_id'=>$invoice,
+            'id'         => $order_id,
+            'invoice_id' => $invoice,
         );
-        if($this->save($data)===false){
+        if ($this->save($data) === false) {
             $this->error = '回写发票失败';
+            $this->_removeGoodsLock($shopping_car_info);
+            $this->rollback();
+            return false;
+        }
+
+        //清空购物车
+        if (D('ShoppingCar')->cleanShoppingCar() === false) {
+            $this->error = '清空购物车失败';
+            $this->_removeGoodsLock($shopping_car_info);
             $this->rollback();
             return false;
         }
         
-        //清空购物车
-        if(D('ShoppingCar')->cleanShoppingCar() ===false){
-            $this->error = '清空购物车失败';
-            $this->rollback();
-            return false;
-        }
-
+        $this->_removeGoodsLock($shopping_car_info);
         $this->commit();
         return true;
-
-
     }
 
     /**
@@ -172,35 +192,33 @@ class OrderInfoModel extends \Think\Model {
         );
         return M('Invoice')->add($data);
     }
-    
+
     /**
      * 获取订单列表
      * @return array
      */
-    public function getPageResult(){
-        $userinfo = login();
-        $cond = array(
-            'status'=>array('gt',0),
-            'member_id'=>$userinfo['id'],
+    public function getPageResult() {
+        $userinfo     = login();
+        $cond         = array(
+            'status'    => array('gt', 0),
+            'member_id' => $userinfo['id'],
         );
         //获取所有的支付类型
         $payment_list = M('Payment')->getField('id,name');
-        $rows = $this->where($cond)->order('inputtime desc')->page(I('get.p',1),C('PAGE_SIZE'))->select();
+        $rows         = $this->where($cond)->order('inputtime desc')->page(I('get.p', 1), C('PAGE_SIZE'))->select();
         //获取到每个订单中商品的logo列表
-        foreach($rows as $key=>$value){
-            $value['total_price'] = my_num_format($value['price']+$value['delivery_price']);
+        foreach ($rows as $key => $value) {
+            $value['total_price']  = my_num_format($value['price'] + $value['delivery_price']);
             $value['payment_name'] = $payment_list[$value['pay_type']];
-            $value['status_name'] = self::$statuses[$value['status']];
+            $value['status_name']  = self::$statuses[$value['status']];
             //取出订单详情
-            $order_info_items = M('OrderInfoItem')->where('order_info_id='.$value['id'])->getField('goods_id,logo');
-            $value['goods_list'] = $order_info_items;
-            $rows[$key] = $value;
-            
+            $order_info_items      = M('OrderInfoItem')->where('order_info_id=' . $value['id'])->getField('goods_id,logo');
+            $value['goods_list']   = $order_info_items;
+            $rows[$key]            = $value;
         }
         return $rows;
     }
 
-    
     /**
      * 更新库存数据
      * @param type $order_id
@@ -210,12 +228,12 @@ class OrderInfoModel extends \Think\Model {
     private function _refreshStock($shopping_car_info) {
         foreach ($shopping_car_info['goods_list'] as $item) {
             //查出所有的商品库存，如果发现库存不够，就返回false
-            $cond = array(
-                'id'=>$item['id'],
-                'stock'=>array('egt',$item['amount']),
+            $cond  = array(
+                'id'    => $item['id'],
+                'stock' => array('egt', $item['amount']),
             );
             //where id=111 and stock>=5
-            if(!$stock = M('Goods')->where($cond)->count()){
+            if (!$stock = M('Goods')->where($cond)->count()) {
                 $this->error = '库存不够';
                 return false;
             }
@@ -224,14 +242,73 @@ class OrderInfoModel extends \Think\Model {
         foreach ($shopping_car_info['goods_list'] as $item) {
             //查出所有的商品库存，如果发现库存不够，就返回false
             $cond = array(
-                'id'=>$item['id'],
+                'id' => $item['id'],
             );
             //where id=111 and stock>=5
-            if(M('Goods')->where($cond)->setDec('stock',$item['amount'])===false){
+            if (M('Goods')->where($cond)->setDec('stock', $item['amount']) === false) {
                 $this->error = '扣库存失败';
                 return false;
             }
         }
         return true;
     }
+
+    /**
+     * 用于检查商品有没有被购买。
+     * @param type $goods_id
+     */
+    private function _checkLock($goods_id) {
+        $redis = getRedis();
+        $key   = 'locked_stock';
+        for ($i = 0; $i < 3;  ++$i) {
+            if ($flag = $redis->sIsMember($key, $goods_id)) {
+                sleep(1);
+            } else {
+                break;
+            }
+        }
+        if($flag){
+            return false;//意味着尝试了三次还没有解锁
+        }else{
+            return true;//已经解锁
+        }
+    }
+    
+    /**
+     * 检查所有的商品id是否被锁定，如果没有就锁定之
+     * @param type $shopping_car_info
+     * @return boolean
+     */
+    private function _checkAllGoodsIsLocked($shopping_car_info){
+        $redis = getRedis();
+        $key   = 'locked_stock';
+        $goods_ids = array();
+        foreach ($shopping_car_info['goods_list'] as $item) {
+            if($this->_checkLock($item['id'])===false){
+                $this->error = '商品太过热门，请稍后再来';
+                return false;
+            }
+            $goods_ids[] = $item['id'];
+        }
+        foreach($goods_ids as $goods_id){
+            $redis->sAdd($key,$goods_id);
+        }
+    }
+    
+    /**
+     * 从redis中解锁
+     */
+    private function _removeGoodsLock($shopping_car_info){
+        $redis = getRedis();
+        $key   = 'locked_stock';
+        foreach ($shopping_car_info['goods_list'] as $item) {
+            $redis->srem($key,$item['id']);
+        }
+    }
+    /**
+     * 1.遍历每个要购买的商品，判断是否被锁定，如果被锁定，直接返回false，不再执行后续的订单创建流程
+     * 2.如果没有被锁定，将商品id存入到redis中，应当创建订单
+     * 3.订单创建完成，将商品id从redis中移除
+     */
+
 }
